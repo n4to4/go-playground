7d3f1f36d183dd38f39eba1411f0896f60ffb283
n4to4 <n4to4k@gmail.com>
2021-10-22 18:47:06 +0900

    snapshot

--- named-args/main.go
+++ named-args/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+)
 
 // https://blog.uptrace.dev/posts/go-functional-options-named-args.html
 
 type myFuncConfig struct {
 	namedArg1 string
 	namedArg2 bool
+	dir       fs.FS
 }
 
 type MyFuncOption func(c *myFuncConfig)
@@ -23,21 +28,43 @@ func WithNamedArg2(val bool) MyFuncOption {
 	}
 }
 
+func WithDir(dir string) MyFuncOption {
+	return func(c *myFuncConfig) {
+		c.dir = os.DirFS(dir)
+	}
+}
+
+func WithDirFS(dir fs.FS) MyFuncOption {
+	return func(c *myFuncConfig) {
+		c.dir = dir
+	}
+}
+
+func DirFromEnv(envName string) MyFuncOption {
+	return func(c *myFuncConfig) {
+		if dir, ok := os.LookupEnv(envName); ok {
+			c.dir = os.DirFS(dir)
+		}
+	}
+}
+
 func MyFunc(arg1, arg2 string, opts ...MyFuncOption) {
 	c := &myFuncConfig{
 		namedArg1: "default value",
 		namedArg2: true,
+		dir:       os.DirFS("/"),
 	}
 	for _, opt := range opts {
 		opt(c)
 	}
 
-	fmt.Printf("%q, %v\n", c.namedArg1, c.namedArg2)
+	fmt.Printf("%+v\n", c)
 }
 
 func main() {
 	MyFunc("dummy", "dummy",
 		WithNamedArg1("this is named arg 1"),
 		WithNamedArg2(false),
+		WithDir("/home/me"),
 	)
 }
