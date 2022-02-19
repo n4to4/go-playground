e1edc268d99de7569b51fbe0659bb75b4551754a
n4to4 <n4to4k@gmail.com>
2022-02-19 09:16:46 +0900

    fuzz

--- fuzzing/main.go
+++ fuzzing/main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	input := "The quick brown fox jumped over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	rev, _ := Reverse(input)
+	doubleRev, _ := Reverse(rev)
 	fmt.Printf("original: %q\n", input)
 	fmt.Printf("reversed: %q\n", rev)
 	fmt.Printf("reversed again: %q\n", doubleRev)
 }
 
-func Reverse(s string) string {
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
+	}
 	b := []rune(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
-	return string(b)
+	return string(b), nil
 }
