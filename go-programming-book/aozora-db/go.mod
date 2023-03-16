b501d6e4c3b601048706fd68715fdbf3b4d3dea3
n4to4 <n4to4k@gmail.com>
2023-03-16 19:04:29 +0900

    wip

--- go-programming-book/aozora-db/go.mod
+++ go-programming-book/aozora-db/go.mod
@@ -2,4 +2,7 @@ module aozora-db
 
 go 1.20
 
-require github.com/mattn/go-sqlite3 v1.14.16
+require (
+	github.com/mattn/go-sqlite3 v1.14.16
+	golang.org/x/text v0.8.0
+)
--- go-programming-book/aozora-db/main.go
+++ go-programming-book/aozora-db/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/text/encoding/japanese"
 )
 
 func main() {
@@ -35,4 +37,27 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+
+	b, err := os.ReadFile("ababababa.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := string(b)
+
+	res, err := db.Exec(
+		`insert into contents (author_id, title_id, title, content) values (?, ?, ?, ?)`,
+		"000879",
+		"14",
+		"あばばばば",
+		content,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	docID, err := res.LastInsertId()
+	_ = docID
 }
